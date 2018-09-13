51cb8b0983a7cef69531b46efbb72115e5543ffb
Sara Kennedy <slkennedy@gmail.com>
2018-09-13 09:19:50 -0400

    Updates

--- src/components/logoLink.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import logo from "../../images/ux-logo.svg"
-
-export default (props) => (
-    <a className="willow-logo-link" href="/" aria-label="go to home page">
-        <img className="willow-logo-link__image" src={logo} alt={props.alt} />
-    </a>
-)
--- src/components/skipNav.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default () => (
-    <div className="willow-skip-nav">
-        <div className="container-fluid">
-            <a href="#mainContent" className="willow-skip-nav__link" aria-label="skip to main content">Skip to Content</a>
-        </div>
-    </div>
-)
