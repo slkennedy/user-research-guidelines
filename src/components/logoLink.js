31692014ec557e1835f10d54dd7622a239160c37
Sara Kennedy <slkennedy@gmail.com>
2018-09-13 10:05:30 -0400

    reverts the logo link changes

--- src/components/logoLink.js
+++ src/components/logoLink.js
@@ -1,9 +1,14 @@
 import React from "react"
 
+import Link from "gatsby-link"
 import logo from "../../images/ux-logo.svg"
 
 export default (props) => (
-    <a className="willow-logo-link" href="/" aria-label="go to home page">
+    <Link 
+        to="/"
+        className="willow-logo-link"
+        aria-label="go to home page"
+    >
         <img className="willow-logo-link__image" src={logo} alt={props.alt} />
-    </a>
+    </Link>
 )
